4dc6a384780f2072a3a07dfa54afa9b69954b5d9
abmael <abmael.bt@gmail.com>
2025-02-14 11:07:36 -0300

    add: error handling if the user does not pass the directory as an argument

--- log_file.sh
+++ log_file.sh
@@ -5,6 +5,11 @@ ORIGIN_DIR="/var/log"
 DEST_DIR="$1"
 FILE_NAME="backup_files_$(date +%Y%m%d_%H%M%S).tar.gz"
 
+if [ -z "$DEST_DIR" ]; then
+    echo "Usage: $0 <mdestination directory>"
+    exit 1
+fi
+
 # create the repository if it does not exist
 mkdir -p "$DEST_DIR"
 
