e1f0bbca6b5bda2be1293c466f2171e2fc7917a1
abmael <abmael.bt@gmail.com>
2025-02-14 11:00:32 -0300

    feat: timestamp when compressing files

--- log_file.sh
+++ log_file.sh
@@ -3,6 +3,7 @@
 # variables
 ORIGIN_DIR="/var/log"
 DEST_DIR="$1"
+FILE_NAME="backup_files_$(date +%Y%m%d_%H%M%S).tar.gz"
 
 # create the repository if it does not exist
 mkdir -p "$DEST_DIR"
@@ -11,4 +12,4 @@ mkdir -p "$DEST_DIR"
 cd $ORIGIN_DIR
 
 # copy and compress the log files to the log-archive directory
-tar -czf "$DEST_DIR"/backup_files.tar.gz *.log
+tar -czf "$DEST_DIR"/"$FILE_NAME" *.log
